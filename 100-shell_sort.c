12209f453a0d16b4c7d76787dbd2a3a3a82a4dbf
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-27 22:27:15 +0200

    fullpush

---
+++ 100-shell_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+/**
+ * shell_sort - hi
+ * @arr: arr
+ * @n: size
+ */
+void shell_sort(int *arr, size_t size) {
+	int n = (int)size, gap = 1;
+	while (gap < n)
+		gap = gap * 3 + 1;
+	while (gap) {
+		for (int i = gap; i < n; ++i) {
+			int tmp = arr[i], j = i;
+			for (; j - gap >= 0 && tmp < arr[j - gap]; j -= gap) {
+				arr[j] = arr[j - gap];
+			}
+			arr[j] = tmp;
+		}
+		gap /= 3;
+		print_array(arr, size);
+	}
+}
